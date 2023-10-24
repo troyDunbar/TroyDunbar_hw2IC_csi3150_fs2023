12f49edaf27f733690c654bc6f020e21a5c80616
Troy Dunbar <troyfdunbar@gmail.com>
2023-10-24 15:35:01 -0400

    finished

---
+++ script.js
@@ -0,0 +1,41 @@
+function showImg1() {
+  let img1 = document.querySelector(".img1");
+  img1.style.visibility = "visible";
+  let hide_img1 = document.querySelector(".hide_img1");
+  hide_img1.style.visibility = "visible";
+}
+
+function hideImg1() {
+  let img1 = document.querySelector(".img1");
+  img1.style.visibility = "hidden";
+  let hide_img1 = document.querySelector(".hide_img1");
+  hide_img1.style.visibility = "hidden";
+}
+
+function showImg2() {
+  let img2 = document.querySelector(".img2");
+  img2.style.visibility = "visible";
+  let hide_img2 = document.querySelector(".hide_img2");
+  hide_img2.style.visibility = "visible";
+}
+
+function hideImg2() {
+  let img2 = document.querySelector(".img2");
+  img2.style.visibility = "hidden";
+  let hide_img2 = document.querySelector(".hide_img2");
+  hide_img2.style.visibility = "hidden";
+}
+
+function showImg3() {
+  let img3 = document.querySelector(".img3");
+  img3.style.visibility = "visible";
+  let hide_img3 = document.querySelector(".hide_img3");
+  hide_img3.style.visibility = "visible";
+}
+
+function hideImg3() {
+  let img3 = document.querySelector(".img3");
+  img3.style.visibility = "hidden";
+  let hide_img3 = document.querySelector(".hide_img3");
+  hide_img3.style.visibility = "hidden";
+}
